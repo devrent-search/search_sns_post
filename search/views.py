f5bce0e4b029103c76d8936bf3438404af35e209
EunseoJ <jes010320@gmail.com>
2023-01-09 16:44:47 +0900

    index.html생성 #settings.py -> installed_apps에 search 추가#views.py -> index 수정 #static/style.css 추가

--- search/views.py
+++ search/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index():
-    pass  # TODO: 01/06 백엔드
-
+def index(request):
+    return render(request, 'search/index.html')  # TODO: 01/06 백엔드
 
 def search():
     pass  # TODO: 01/06 백엔드
