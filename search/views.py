c4d897017f1585c77dae2cf995e0ef6cd650c188
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2023-01-09 22:53:43 +0900

    1.9 oshmos branch for merge with pull-request

--- search/views.py
+++ search/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 
 
 def index(request):
+    # 여기다가 코드 입력하시면 됩니다.
     return render(request, 'search/index.html')
 
 
 def search(request):
+    # 여기다가 코드 입력하시면 됩니다.
     return render(request, 'search/search.html')
