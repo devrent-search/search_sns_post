69a2c39553a223d4a6888fbf745f1b7798777531
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2023-01-09 13:47:28 +0900

    1.9 회의 준비

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'search',
 ]
 
 MIDDLEWARE = [
--- search/views.py
+++ search/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index():
-    pass  # TODO: 01/06 백엔드
+
+def index(request):
+    return render(request, 'base.html')
 
 
-def search():
-    pass  # TODO: 01/06 백엔드
+def search(request):
+    return render(request, 'search/search.html')
